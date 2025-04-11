7ccd6d473424a2a5a65a3fb81c4d13269eddb1cc
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-11 17:44:29 +0800

    feat: add home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,28 @@
 <header>
-  <h1>Navbar</h1>
+  <div>
+    <h1>Sample LEAP Workshop</h1>
+  </div>
+  <div class="orgTitle">
+    <h1>LSCS</h1>
+  </div>
 </header>
+
+<slot></slot>
+
+<style>
+  header {
+    background-color: #d0273b;
+    color: white;
+    padding: 1.5em 3em;
+    display: flex;
+    justify-content: space-between;
+  }
+  header > div > h1 {
+    font-weight: bold;
+    font-size: 2em;
+  }
+
+  .orgTitle {
+    padding-right: 2.5em;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import "../global.css";
+</script>
+
+<div class="page">
+  <div class="workshop">
+    <h1>This is a Sample Workshop!</h1>
+    <p>Register here!</p>
+    <a href="">Workshop</a>
+  </div>
+</div>
+
+<style>
+  .workshop {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+    padding-top: 8em;
+  }
+
+  .workshop > h1 {
+    font-size: 2.8em;
+  }
+
+  .workshop > p {
+    font-size: 1.5em;
+  }
+
+  .workshop > a {
+    background-color: #d0273b;
+    color: white;
+    padding: 1em 2em;
+    border-radius: 0.2em;
+    margin-top: 0.5em;
+    outline: none;
+    font-size: 1em;
+  }
+</style>
