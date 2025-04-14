06a5a2ed2600566f778ced9830d4d321f7acb07d
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-14 12:54:48 +0800

    feat: add a google forms and inactivity per 3 seconds and close class feature

--- src/routes/workshop/+page.svelte
+++ src/routes/workshop/+page.svelte
@@ -1,24 +1,75 @@
 <script>
-  import "../../global.css";
-  let registerd = 0;
+  import '../../global.css'
+  let registered = 0
+  let isActive = true
+  let closed = false
+
+  $: {
+    if (registered >= 5) {
+      closed = true
+      isActive = false
+    }
+  }
+
+  const register = () => {
+    if (!closed) {
+      isActive = !isActive
+      window.open(
+        'https://docs.google.com/forms/d/e/1FAIpQLScgksP_Clr_jtIRtiE8z1Y5c4OiTxTaERh9tAEGlisNEFSkHg/viewform?usp=dialog',
+      )
+      registered++
+      setTimeout(() => {
+        isActive = !isActive
+      }, 3000)
+    }
+  }
 </script>
 
 <div>
-  <h2>Sample Workshop #1</h2>
+  <span>
+    <h2>Sample Workshop #1</h2>
+    <p class={closed ? '' : 'hide'}>Closed</p>
+  </span>
   <p>Workshop Title: Stress testing...</p>
-  <p>Number of Registered Students: {registerd}</p>
-  <button>Register</button>
+  <p>Number of Registered Students: {registered}</p>
+  <button
+    on:click={register}
+    disabled={!isActive}
+    class={`${isActive ? '' : 'inActive'} ${closed ? 'inActive' : ''}`}
+    >Register</button
+  >
 </div>
 
 <style>
+  span {
+    display: flex;
+    gap: 1em;
+    margin-bottom: 1em;
+  }
   div {
     padding: 1.5em;
     font-size: 1.4em;
   }
 
+  .hide {
+    display: none;
+  }
+  span > p {
+    margin-top: 0.3em;
+    background-color: #d0273b;
+    color: white;
+    border-radius: 1em;
+    padding: 0.2em;
+    font-size: 1em;
+  }
+
   div > p {
     margin-top: 0.4em;
     font-size: 1.2em;
+  }
+
+  .inActive {
+    background-color: gray;
   }
   div > button {
     background-color: #d0273b;
